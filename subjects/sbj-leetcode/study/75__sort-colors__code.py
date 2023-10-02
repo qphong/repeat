18df74e4d1eea06836bad5b9b97f7272bb593b10
Nguyen Quoc Phong <qphongmp@gmail.com>
2023-10-01 22:51:46 -0400

    add some comments to the script and update the demo video

---
+++ subjects/sbj-leetcode/study/75__sort-colors__code.py
@@ -0,0 +1,2 @@
+# This is neovide we can start coding the solution here
+# it is a file save under the study direction
---
+++ subjects/sbj-leetcode/study/75__sort-colors__node.md
@@ -0,0 +1,1 @@
+here we can add some note, this file is also saved under the study directory
